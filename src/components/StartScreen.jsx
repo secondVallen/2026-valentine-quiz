d0c88ff55e81ebfbb547184ff76a65a7b060d6f8
secondVallen <vallenn1406@gmail.com>
2026-02-13 23:58:36 +0900

    Delete src directory

--- src/components/StartScreen.jsx
+++
@@ -1,8 +0,0 @@
-export default function StartScreen({ onStart }) {
-  return (
-    <div className="container">
-      <h1>Let's see how well you remember random things about us 👴🏻👵🏻</h1>
-      <button onClick={onStart}>Start</button>
-    </div>
-  );
-}
